6d0c3c161ca913aeb11cf39b38417376f88cb314
Julian <julianfinelli97@gmail.com>
2025-02-17 08:22:53 -0300

    chore: last changes in api

---
+++ src/models/gisDetails/index.js
@@ -0,0 +1,55 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../../db/index.js";
+import Item from "../items/index.js";
+
+const GisDetail = sequelize.define(
+    "GisDetail",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        id_item: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: Item,
+                key: "id",
+            },
+            onUpdate: "CASCADE",
+            onDelete: "SET NULL",
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        opening_hours: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        tel: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        smm: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        management: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    {
+        tableName: "gis_details",
+        timestamps: true,
+        paranoid: true,
+    }
+);
+
+export default GisDetail;
--- src/models/index.js
+++ src/models/index.js
@@ -1,35 +1,51 @@
 import Section from "./sections/index.js";
 import Theme from "./themes/index.js";
 import Item from "./items/index.js";
+import GisDetail from "./gisDetails/index.js";
 
 // Una sección puede tener muchos temas, pero un tema pertenece a una sola sección.
 Section.hasMany(Theme, {
   foreignKey: "id_section",
   as: "themes",
   onUpdate: "CASCADE",
-  onDelete: "SET NULL",
+  onDelete: "CASCADE",
 });
 
 Theme.belongsTo(Section, {
   foreignKey: "id_section",
   as: "section", 
   onUpdate: "CASCADE",
-  onDelete: "SET NULL",
+  onDelete: "CASCADE",
 });
 
 // Un tema puede tener muchos ítems, pero un ítem pertenece a un solo tema.
 Theme.hasMany(Item, {
   foreignKey: "id_theme",
-  as: "items", // Alias para acceder a los ítems desde un tema
+  as: "items",
   onUpdate: "CASCADE",
-  onDelete: "SET NULL",
+  onDelete: "CASCADE",
 });
 
 Item.belongsTo(Theme, {
   foreignKey: "id_theme",
   as: "theme", 
   onUpdate: "CASCADE",
-  onDelete: "SET NULL",
+  onDelete: "CASCADE",
 });
 
-export { Section, Theme, Item };
+// Un ítem puede tener un solo detalle GIS, pero un detalle GIS pertenece a un solo ítem.
+Item.hasOne(GisDetail, {
+  foreignKey: "id_item",
+  as: "gisDetail",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE",
+});
+
+GisDetail.belongsTo(Item, {
+  foreignKey: "id_item",
+  as: "item",
+  onUpdate: "CASCADE",
+  onDelete: "CASCADE",
+});
+
+export { Section, Theme, Item, GisDetail };
